3a5b84d684add1b2198bdf8ecb9922b212eff8e0
saul <eternx@163.com>
2018-05-18 15:36:42 +0800

    <005> add(LogisticsRecordController,LogisticsRecord)

---
+++ src/main/java/xin/saul/jxmall/controller/LogisticsRecordController.java
@@ -0,0 +1,31 @@
+package xin.saul.jxmall.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import xin.saul.jxmall.entity.LogisticsRecord;
+
+@RestController
+public class LogisticsRecordController {
+
+    public static final String LOGISTICS_RECORD_ID = "logisticsRecords/{id}";
+    public static final String LOGISTICS_RECORDS_ID_ORDERS_ORDER_ID = "logisticsRecords/{id}/orders/{orderId}";
+    public static final String LOGISTICS_STATUS_SHIPPING = "logisticsStatus=shipping";
+    public static final String LOGISTICS_STATUS_SIGNED = "logisticsStatus=signed";
+
+    @GetMapping(LOGISTICS_RECORD_ID)
+    public LogisticsRecord find(Long id){
+        return new LogisticsRecord();
+    }
+
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @PutMapping(value = LOGISTICS_RECORDS_ID_ORDERS_ORDER_ID, params = LOGISTICS_STATUS_SHIPPING)
+    public void shipping(@PathVariable Long id,@PathVariable Long orderId){
+        System.out.println("shopping");
+    }
+
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @PutMapping(value = LOGISTICS_RECORDS_ID_ORDERS_ORDER_ID,params = LOGISTICS_STATUS_SIGNED)
+    public void signed(@PathVariable Long id,@PathVariable Long orderId){
+        System.out.println("signed");
+    }
+}
---
+++ src/main/java/xin/saul/jxmall/entity/LogisticsRecord.java
@@ -0,0 +1,6 @@
+package xin.saul.jxmall.entity;
+
+public class LogisticsRecord {
+    Long id;
+    Long orderId;
+}
