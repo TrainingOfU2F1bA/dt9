b6c71274f988c7544aa1d1c2087ba010a214d841
saul <eternx@163.com>
2018-05-19 13:12:48 +0800

    add(V2.*.sql,V3.*.sql):add test data

--- src/main/java/xin/saul/jxmall/dao/ProductDao.java
+++ src/main/java/xin/saul/jxmall/dao/ProductDao.java
@@ -4,10 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import xin.saul.jxmall.entity.Product;
 
+import java.util.List;
+
 @Repository
 public interface ProductDao extends JpaRepository<Product,Long> {
 
-    Object findByNameLike(String name);
+    List<Product> findByNameLike(String name);
 
-    Object findByNameLikeAndDescriptionLike(String name, String description);
+    List<Product> findByNameLikeAndDescriptionLike(String name, String description);
 }
---
+++ src/main/resources/db/migration/V3__Insert_data.sql
@@ -0,0 +1,1 @@
+insert into Product (name, description, price) values ('方便面', '康师傅', 9);
